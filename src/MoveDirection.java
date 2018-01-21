fc4cf4bdec40499c48ba420c77a4b492f8342daf
Jeremy Young <jedayoung@gmail.com>
2018-01-21 08:54:02 -0600

    Refactored MoveDirection findPosition code

--- src/MoveDirection.java
+++ src/MoveDirection.java
@@ -19,7 +19,7 @@ public class MoveDirection {
     return horizontal;
   }
 
-  private List<Integer> findAllSquarePositions(int fromPosition, int limit) {
+  private List<Integer> findAllPositions(int fromPosition, int limit) {
     List<Integer> positions = new ArrayList<Integer>();
 
     int amount = 1;
@@ -33,18 +33,35 @@ public class MoveDirection {
     return positions;
   }
 
-  public Moves findUnblockedMoves(int fromPosition, Board board, Color color, CheckSquare canMove, int limit) {
-    Moves unblockedMoves = new Moves();
+  private List<Integer> filterUnblockedPositions(
+    List<Integer> positionsInDirection, 
+    Board board, 
+    Color color, 
+    CheckSquare canMove) 
+  {
+    List<Integer> unblockedPositions = new ArrayList<Integer>();
 
-    List<Integer> positionsInDirection = findAllSquarePositions(fromPosition, limit);
     for (int toPosition : positionsInDirection) {
       Square square = board.findSquare(toPosition);
       if (canMove.test(square, color)) {
-        unblockedMoves.add(new Move(fromPosition, toPosition));
+        unblockedPositions.add(toPosition);
       }
       if (new CheckSquareIsBlocking().test(square, color)) {
         break;
       }
+    }
+
+    return unblockedPositions;
+  }
+
+  public Moves findUnblockedMoves(int fromPosition, Board board, Color color, CheckSquare canMove, int limit) {
+    Moves unblockedMoves = new Moves();
+
+    List<Integer> positionsInDirection = findAllPositions(fromPosition, limit);
+    List<Integer> unblockedPositions = filterUnblockedPositions(positionsInDirection, board, color, canMove);
+
+    for (int toPosition : unblockedPositions) {
+      unblockedMoves.add(new Move(fromPosition, toPosition));
     }
 
     return unblockedMoves;
