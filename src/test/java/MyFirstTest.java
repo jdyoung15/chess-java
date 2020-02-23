e41eeccdf67a1c0c7e8918e63dbf1499bfc6509c
Jeremy Young <jedayoung@gmail.com>
2020-02-22 18:56:21 -0800

    Create working stub junit test

---
+++ src/test/java/MyFirstTest.java
@@ -0,0 +1,14 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class MyFirstTest {
+
+  @Test
+  public void firstTest() {
+    assertTrue(false);
+  }
+
+}
